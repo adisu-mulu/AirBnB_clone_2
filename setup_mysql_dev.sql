2d1738bf0ee2bc4d8dd24ea04afd7948fc473de2
adisu-mulu <kulhabesh31@gmail.com>
2024-07-01 22:45:58 +0300

    modified setup

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,9 +1,9 @@
---- creating user if not exist
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED WITH mysql_native_password BY 'hbnb_dev_pwd';
---- creating database if it does not exist
+
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
---- granting all privileges
+
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
---- granting select privileges
+
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;

ce7a0d74fab494686baa1bb51aa9fee28a0f6061
adisu-mulu <kulhabesh31@gmail.com>
2024-07-27 11:41:26 +0300

    pycodestyle fix

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -20,7 +20,7 @@ def HBNB():
 @app.route('/c/<text>', strict_slashes=False)
 def HBNB_text(text):
     """displaying variable rules"""
-    return 'C '+ text.replace('_',' ')
+    return 'C ' + text.replace('_', ' ')
 
 
 if __name__ == '__main__':

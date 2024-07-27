b3d17f2a99da4116c0f7cbfab79c3b5bf0fa421d
adisu-mulu <kulhabesh31@gmail.com>
2024-07-27 12:02:18 +0300

    adding 4-number route

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""This module defines a flask instance """
+from flask import Flask
+from markupsafe import escape
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_HBNB():
+    """Function to be called on the route /"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def HBNB():
+    """function to be called on the /hbnb"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def HBNB_text(text):
+    """displaying variable rules"""
+    return 'C ' + text.replace('_', ' ')
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def HBNB_python(text='is cool'):
+    """displaying variable rules with optional default value"""
+    return 'Python ' + text.replace('_', ' ')
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def imanumber(n):
+    """display “n is a number” only if n is an integer"""
+    return "{:d} is a number".format(n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')

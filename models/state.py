f5884d421d39f0e0c85453e22eef1bbfbc04ea13
adisu-mulu <kulhabesh31@gmail.com>
2024-07-27 15:28:30 +0300

    updated pep8

--- models/state.py
+++ models/state.py
@@ -6,12 +6,13 @@ import os
 from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
 from sqlalchemy.orm import relationship
 
+
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
     if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        cities = relationship("City", backref ="state")
+        cities = relationship("City", backref="state")
     else:
         @property
         def cities(self):

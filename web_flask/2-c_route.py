b789d2c3ccd4601a198cac06256d98af60650d9f
adisu-mulu <kulhabesh31@gmail.com>
2024-07-27 11:30:28 +0300

    adding 2-c route

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""This module defines a flask instance """
+from flask import Flask
+from markupsafe import escape
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_HBNB():
+    """Function to be called on the route /"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def HBNB():
+    """function to be called on the /hbnb"""
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def HBNB_text(text):
+    """displaying variable rules"""
+    return f'C {escape(text)}'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
